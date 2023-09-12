1a7a8fd01a833b6e88b6c1b116e1375a13af6b47
deiivid10 <143755846+deiivid10@users.noreply.github.com>
2023-09-12 16:56:34 -0600

    Update Python2.py

--- Python2.py
+++ Python2.py
@@ -17,3 +17,54 @@ int("Hola como estas",nombre)
 #=====================================================================
 y = 5000000000000000000000000000000000000000000
 print(y)
+
+#==========================================
+# La función int() cambia strings y floats a enteros
+#==========================================
+número = int(input("Dame tu edad: "))
+type(numero)
+
+#==========================================
+# La notación científica de flotantes  utiliza e o E
+#===========================================
+# 1.2 x 10^{-9}
+#===========================================
+y = float("14.3")
+print(y)
+
+#=========================================================
+# Los complejos se escriben con la raiz de menos uno
+# j Siempre con su número como prefijo 
+# No acepta la j suelta
+#=========================================================
+z = 1+1j
+
+# Suma +
+# Resta -
+# Multiplicación *
+# División /
+# Módulo%
+# Exponente **
+# // Función Piso
+# Funciones para transformar numeros int() float() complex()
+# Operaciones abs() round() pow()
+
+print(round(3.14159,4))
+
+#=========================
+# Strings de varias lineas 
+#===========================
+parrafo = """ En el bosque de la china 
+ la chinita se perdio """
+print(parrafo)
+#================================
+n=len(parrafo)
+print(n)
+
+#===================================================
+# Las letras en un string ocupan lugares como un arreglo 
+#  (También de atrás adelante comenzando en -1 el ultimo)
+#==========================================================
+palabra = "hola"
+print(palabra[0])
+print(palabra[-4])
