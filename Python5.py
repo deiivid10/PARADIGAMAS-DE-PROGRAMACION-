df69429edc09830e2d2487cf9a48ec9d9e75e358
deiivid10 <143755846+deiivid10@users.noreply.github.com>
2023-09-14 16:33:12 -0600

    Update Python5.py

--- Python5.py
+++ Python5.py
@@ -1,1 +1,30 @@
+#========================================
+# Juan David Garcia Vargas
+#========================================
+# Paradigmas de Programacion
+# Matematica Algoritmica 
+# ESFM-IPN 
+#========================================
+
+#================================
+# Condicionales 
+#=================================
+precio = 50
+#----------------------
+# Si esto......
+#----------------------
+if precio < 50:
+  print("El precio es mayor a 50")
+#-------------------------------------
+# Si nada de lo anterior..........
+#--------------------------------------------
+else:
+  print("El precio es 50")
+
+precio = 50
+cantidad = 5
+total = precio*cantidad
+#=====================================
+# Condicionales anidados 
+#==========================================
 
