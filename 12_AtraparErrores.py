65e64a70d6ec83a4f56ef506f36588583a9019b3
deiivid1272@gmail.com <deiivid1272@gmail.com>
2023-10-26 15:58:17 -0600

    Trabajo 12

---
+++ 12_AtraparErrores.py
@@ -0,0 +1,47 @@
+#========================================
+# Juan David Garcia Vargas
+#========================================
+# Paradigmas de Programacion
+# Matematica Algoritmica 
+# ESFM-IPN 
+#========================================
+
+#=============================================================
+# La clase ClienteBancario esta en el subdirectorio 
+# aplicacion/banco
+#===============================================================
+from cliente_bancario import ClienteBancario
+
+#=========================================================
+# try: intenta (correr las instrucciones)
+# except: atrapar el error en una variable e
+# e se puede convertir a string
+#===========================================================
+
+#============================================
+#Error por sacar mas dinero del que tiene
+#================================================
+try: 
+  cliente = ClienteBancario("Jaime Andrade", "Hernandez Sanchez", 28, 0.0)
+  cliente.guardarDinero(300)
+  print(cliente.imprimirInfo())
+  cliente.retirarDinero(400)
+  print(cliente.imprimirInfo())
+#====================================================
+# Exception es el objeto mas general de error 
+#======================================================
+except Exception as e:
+    print("Error: " +str(e)) 
+
+#=======================================
+# Error por usar un atributo privado
+#=========================================
+try:
+  print(cliente.__nombres)
+except Exception as ex:
+  print("Error: " + str(ex))
+
+#===================================
+# Forma correcta 
+#=====================================
+print(cliente.nombres)
